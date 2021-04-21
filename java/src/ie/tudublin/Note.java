567274ddc3bef338647d91becd88d8e343ce703f
William Greene Hill <williamgreenehill@Williams-MacBook-Air.local>
2021-04-21 16:20:53 +0100

    Implemented the Note Class...

---
+++ java/src/ie/tudublin/Note.java
@@ -0,0 +1,13 @@
+public class Note {
+    private char note;
+    private int duration;
+
+    public Note(char note, int duration) {
+        this.note = note;
+        this.duration = duration;
+    }
+
+    public String toString() {
+        return note + " " + duration;
+    }
+}
